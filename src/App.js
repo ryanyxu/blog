ed0db524e9f54678d39ba6f07b2a9ade5186c54a
Ryan Xu <rxu@brandeis.edu>
2020-12-13 13:59:46 -0500

    allow for saving and loading posts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from 'react';
 import {
   Editor,
   EditorState,
   RichUtils,
   getDefaultKeyBinding,
-  KeyBindingUtil} from 'draft-js';
+  KeyBindingUtil,
+  convertFromRaw,
+  convertToRaw} from 'draft-js';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useParams,
+    withRouter
+  } from "react-router-dom";
 
 import 'draft-js/dist/Draft.css';
 import './App.css'
 
 import firebase from 'firebase';
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/:id" children={<Blog/>} />
+      </Switch>
+    </Router>
+  );
+}
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.addItem = this.addItem.bind(this);
+    //const {id}= useParams()
+    var id = 'ryan'
+    this.state = {posts: [], id: id}
+    firebase.database().ref("user").child(id).on('child_added', (snapshot) => {
+      this.setState({posts: [...this.state.posts, {postId: snapshot.key, content: EditorState.createWithContent(convertFromRaw(JSON.parse(snapshot.val().content)))}]})
+    })
+    /* not needed yet
+    database.ref('items').on('child_removed', (data) => {
+      console.log('removed')
+      for (var i = 0; i < this.state.blogs.length; i++) {
+        if (this.state.blogs[i].key == data.key) {
+          this.state.blogs.splice(i, 1)
+        }
+      }
+      this.setState({})
+    })
+    */
+  }
+  /*
+  addPost(e) {
+    if (e.key === "Enter") {
+      addItem(e.target.value);
+      e.target.value = "";
+    }
+  }
+
+  deleteItem(itemKey) {
+    removeItem(itemKey);
+  }
+  */
 
 
-function App() {
-  var database = firebase.database();
+  render() {
+    return <>{this.state.posts.map((post) => <Post id={this.state.id} post = {post}/>)}</>
+  }
+}
 
+
+const Post = (props) => {
   const [editorState, setEditorState] = React.useState(
-    () => EditorState.createEmpty(),
+    () => props.post.content
   );
 
   const handleKeyCommand = (command, editorState) => {
@@ -35,14 +92,15 @@ function App() {
   const {hasCommandModifier} = KeyBindingUtil
 
   const keyBinding = (e) => {
-    if (e.keyCode === 83 /* `S` key */ && hasCommandModifier(e)) {
-      console.log('saved');
-      database.ref("user").child('ryan').push('hi');
+    if (e.keyCode === 75 /* `S` key */ && hasCommandModifier(e)) {
+      var postId = props.post.postId
+      firebase.database().ref("user").child(props.id).set({[postId]: {
+          date: Date.now().toString(),
+          content: JSON.stringify(convertToRaw(editorState.getCurrentContent()))
+        }})
     }
     return getDefaultKeyBinding(e);
   }
-
-
 
   return <Editor
     editorState={editorState}
