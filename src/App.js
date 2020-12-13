8585e423bc84c3545645fbdc395bf23b76960564
Ryan Xu <rxu@brandeis.edu>
2020-12-13 10:46:23 -0500

    initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import {
+  Editor,
+  EditorState,
+  RichUtils,
+  getDefaultKeyBinding,
+  KeyBindingUtil} from 'draft-js';
+
 import 'draft-js/dist/Draft.css';
+import './App.css'
+
+import firebase from 'firebase';
+
+
 
 function App() {
+  var database = firebase.database();
+
   const [editorState, setEditorState] = React.useState(
     () => EditorState.createEmpty(),
   );
 
-  return <Editor editorState={editorState} onChange={setEditorState} />;
+  const handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      setEditorState(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+  const {hasCommandModifier} = KeyBindingUtil
+
+  const keyBinding = (e) => {
+    if (e.keyCode === 83 /* `S` key */ && hasCommandModifier(e)) {
+      console.log('saved');
+      database.ref("user").child('ryan').push('hi');
+    }
+    return getDefaultKeyBinding(e);
+  }
+
+
+
+  return <Editor
+    editorState={editorState}
+    onChange={setEditorState} 
+    handleKeyCommand={handleKeyCommand}
+    keyBindingFn={keyBinding}
+  />;
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBeezw-yyxDhs1jurdGo-fCfgywb50Wdjo",
+  authDomain: "rxublog.firebaseapp.com",
+  databaseURL: "https://rxublog-default-rtdb.firebaseio.com",
+  projectId: "rxublog",
+  storageBucket: "rxublog.appspot.com",
+  messagingSenderId: "1089305203848",
+  appId: "1:1089305203848:web:0d7ff7266c4c1d96b2fb3d"
+};
+
+firebase.initializeApp(config)
 
 ReactDOM.render(
   <React.StrictMode>
